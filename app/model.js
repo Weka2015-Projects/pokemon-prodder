4afb9475992d9dc4594fc4be741dcfe3cf37c2aa
irene robb <email": null>
2016-01-19 18:27:21 +1300

    adding 6 new pokemon

--- app/model.js
+++ app/model.js
@@ -27,5 +27,29 @@ export class Model {
         $('.charmeleon').removeClass('charmeleon').addClass('charizard')
         this.mastered++
       }
-}
+      if(this.counter === 30){
+        $('.charizard').removeClass('charizard').addClass('squirtle')
+        this.mastered++
+      }
+      if(this.counter === 35){
+        $('.squirtle').removeClass('squirtle').addClass('wartortle')
+        this.mastered++
+      }
+      if(this.counter === 40){
+        $('.wartortle').removeClass('wartortle').addClass('blastoise')
+        this.mastered++
+      }
+      if(this.counter === 45){
+        $('.blastoise').removeClass('blastoise').addClass('caterpie')
+        this.mastered++
+      }
+      if(this.counter === 50){
+        $('.caterpie').removeClass('caterpie').addClass('metapod')
+        this.mastered++
+      }
+      if(this.counter === 55){
+        $('.metapod').removeClass('metapod').addClass('butterfree')
+        this.mastered++
+      }
+  }
 }
